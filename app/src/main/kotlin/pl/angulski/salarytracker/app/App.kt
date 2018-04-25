535cb7c4c1b65f0360a5d56e8cd96799343bd2ac
Mateusz Angulski <mateusz.angulski@assertis.co.uk>
2018-04-25 19:38:39 +0200

    Remove comments, todo list is moved to github issues

--- app/src/main/kotlin/pl/angulski/salarytracker/app/App.kt
+++ app/src/main/kotlin/pl/angulski/salarytracker/app/App.kt
@@ -4,14 +4,6 @@ import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 import pl.angulski.salarytracker.app.di.DaggerAppComponent
 
-/**
- * TODO
- * - Implement coroutines in use cases
- * - Set percent
- * - Show month salary
- * - Show average monthly salary
- */
-
 class App : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out App> {
         return DaggerAppComponent.builder().create(this)
